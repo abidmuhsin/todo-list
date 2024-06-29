1dba4cdcbadcf7b3bdb30051433fce799dd2f653
Abid <abidmhsn@gmail.com>
2024-06-29 17:04:09 +0800

    initial commit

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,15 @@
+import { AiFillDelete } from "react-icons/ai"; 
+import { AiFillCheckCircle } from "react-icons/ai"; 
+import React from 'react'
+
+export const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
+  return (
+    <div className='flex items-center my-3 gap-2'>
+        <div  onClick={()=>{toggle(id)}}className="flex flex-1 items-center cursor-pointer">
+            {isComplete && <AiFillCheckCircle className="text-2xl"/>}
+            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through":""}`}>{text}</p>
+        </div>
+        <AiFillDelete onClick={()=>{deleteTodo(id)}} className="text-2xl cursor-pointer"/>
+    </div>
+  )
+}
